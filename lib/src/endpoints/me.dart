09d8bde7949c69eae2359eb9d58ceed90308bea5
Robert Moller <33206528+roberthmoller@users.noreply.github.com>
2023-02-01 06:10:52 +0100

    Update me.dart

--- lib/src/endpoints/me.dart
+++ lib/src/endpoints/me.dart
@@ -15,6 +15,24 @@ class Me extends EndpointPaging {
 
     return User.fromJson(map);
   }
+  
+  
+  Future<Player> play() async {
+    var jsonString = await _api._put('$_path/player/play');
+
+    var map = json.decode(jsonString);
+    return Player.fromJson(map);
+  }
+
+  Future<Player> pause({String deviceId}) async {
+    var jsonString = await _api._put('$_path/player/pause?' +
+        _buildQuery({
+          'deviceId': deviceId,
+        }));
+
+    var map = json.decode(jsonString);
+    return Player.fromJson(map);
+  }
 
   /// Endpoint /v1/me/following only supports "artist" type at the moment.
   /// needs 'user-follow-read' scope
